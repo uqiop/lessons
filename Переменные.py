25fde0d9f9890fd2aabd7e5af7272d609145542e
uqiop <n11121222@gmail.com>
2024-11-11 17:36:04 +0500

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,5 @@
+Total_tasks = 12
+hours_spent = 1.5
+Course = "Python"
+average_execution_time = "0.125 часа"
+print ("Курс:",Course,",","всего задач:",Total_tasks,",","затрачено часов:",hours_spent,",","среднее время выполнения",average_execution_time  )
