ebc460ef156a391cbf3a5560789d5d638d867e15
uqiop <n11121222@gmail.com>
2024-11-22 17:53:24 +0500

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,21 @@
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+def is_contains(string, list_to_search):
+    count_calls()
+    string_lower = string.lower()
+    return any(string_lower in item.lower() for item in list_to_search)
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))    # No matches
+
+# Выводим количество вызовов
+print(calls)
